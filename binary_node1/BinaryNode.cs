30f620fbd74d55430ae761550fd44fda648e2f6c
Adam Gutierrez <adam.gutierrez@hyland.com>
2023-01-09 14:51:04 -0500

    Add project files.

---
+++ binary_node1/BinaryNode.cs
@@ -0,0 +1,46 @@
+﻿namespace binary_node1
+{
+	class BinaryNode<T>
+	{
+		T Value;
+		BinaryNode<T>? LeftChild;
+		BinaryNode<T>? RightChild;
+
+		public BinaryNode(T value)
+		{
+			Value = value;
+			LeftChild = null;
+			RightChild = null;
+		}
+
+		public void AddLeft(BinaryNode<T> leftNode)
+		{
+			LeftChild = leftNode;
+		}
+
+		public void AddRight(BinaryNode<T> rightNode)
+		{
+			RightChild = rightNode;
+		}
+
+		public override string ToString()
+		{
+			if (LeftChild is null && RightChild is null)
+			{
+				return $"{Value}: null null";
+			}
+
+			if (LeftChild is null)
+			{
+				return $"{Value}: null {RightChild.Value}";
+			}
+
+			if (RightChild is null)
+			{
+				return $"{Value}: {LeftChild.Value} null";
+			}
+
+			return $"{Value}: {LeftChild.Value} {RightChild.Value}";
+		}
+	}
+}
---
+++ binary_node1/Program.cs
@@ -0,0 +1,27 @@
+﻿using binary_node1;
+
+BinaryNode<string> rootNode = new BinaryNode<string>("Root");
+BinaryNode<string> aNode = new BinaryNode<string>("A");
+BinaryNode<string> bNode = new BinaryNode<string>("B");
+BinaryNode<string> cNode = new BinaryNode<string>("C");
+BinaryNode<string> dNode = new BinaryNode<string>("D");
+BinaryNode<string> eNode = new BinaryNode<string>("E");
+BinaryNode<string> fNode = new BinaryNode<string>("F");
+
+rootNode.AddLeft(aNode);
+rootNode.AddRight(bNode);
+
+aNode.AddLeft(cNode);
+aNode.AddRight(dNode);
+
+bNode.AddRight(eNode);
+
+eNode.AddLeft(fNode);
+
+Console.WriteLine(rootNode.ToString());
+Console.WriteLine(aNode.ToString());
+Console.WriteLine(bNode.ToString());
+Console.WriteLine(cNode.ToString());
+Console.WriteLine(dNode.ToString());
+Console.WriteLine(eNode.ToString());
+Console.WriteLine(fNode.ToString());
